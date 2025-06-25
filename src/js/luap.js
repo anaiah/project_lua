61c63a594f64be80e84d047f2cce5020eae8a928
anaiah <carlodominguez@yahoo.com>
2025-06-26 01:33:28 +0800

    Update luap.js

--- src/js/luap.js
+++ src/js/luap.js
@@ -3,7 +3,7 @@
     function handleIntersection(entries, observer) {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.add('visible'); 
+                entry.target.classList.add('visible');  
             } else {
                 entry.target.classList.remove('visible');
             }
